2fc5ad6be8f654fa0b82712d7aa96574b46955a1
Max Junior Soncco Mamani <msonccoma@unsa.edu.pe>
2025-05-10 19:14:20 -0500

    urls de la aplicacion

---
+++ backend/core_app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views # Importarás tus vistas aquí
+
+# urlpatterns = [
+#     path('ruta-ejemplo/', views.MiVista.as_view(), name='mi-vista'),
+# ]
+# Se completa más adelante
+urlpatterns = []
