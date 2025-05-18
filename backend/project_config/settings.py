0e001753d556eef25601ff5f13fb7ff682a5dbe3
JoseGordilloMendoza <jgordillome@unsa.edu.pe>
2025-05-18 17:56:34 -0500

    serializers

--- backend/project_config/settings.py
+++ backend/project_config/settings.py
@@ -73,10 +73,11 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ),
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
 }
+
 from datetime import timedelta
 
 SIMPLE_JWT = {
