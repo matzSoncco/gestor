0aa5ca62c0278fc9eadc52b678c501dcbae65110
Max Junior Soncco Mamani <msonccoma@unsa.edu.pe>
2025-08-24 22:53:02 -0500

    Modifica el timeout de la peticion para no cortar las peticiones muy rápido, trata de solucionar el error de credenciales incorrectas en el frontend cuando el servidor está dormido

--- frontend/src/services/authService.ts
+++ frontend/src/services/authService.ts
@@ -3,7 +3,7 @@ import { useAuthStore } from "@/stores/auth";
 
 const api: AxiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
-  timeout: 20000,
+  timeout: 60000,
   headers: { "Content-Type": "application/json" },
 });
 
